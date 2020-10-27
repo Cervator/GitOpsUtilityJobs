54c50d0721b74fee06e8f1a095f3b2b20a399703
Cervator <cervator@gmail.com>
2020-10-27 16:49:05 -0500

    Left this offline too long, hopefully it still works!

--- src/jobs/Incidizer.groovy
+++ src/jobs/Incidizer.groovy
@@ -9,6 +9,29 @@ pipelineJob(jobName) {
                 @Library('HoboHealthCheck') _
 
                 pipeline {
+                    scm {
+                        git {
+                            remote {
+                                // TODO: Consider varying this env var to point it at the repo holding the desired manifest
+                                url(System.getenv("GITOPS_MANIFEST_REPO"))
+                                // TODO: If using private repos either replace with a username that has access to the DSL repo or make the env var
+                                // Note that in a brand new generated Jenkins the credential will need to be created before this will work
+                                def dslRepoCred = System.getenv("GIT_CREDENTIAL")
+                                if (dslRepoCred != null && !dslRepoCred.equals("")) {
+                                    println '"GIT_CREDENTIAL" was set - using credential: ' + dslRepoCred
+                                    credentials(dslRepoCred)
+                                } else {
+                                    println 'No "GIT_CREDENTIAL" set, so assuming public repository'
+                                }
+
+                              // This is an optional ability to use branches in the dsl/manifest repos if desired
+                              def targetBranch = getBinding().getVariables()["GITOPS_PLATFORM_LEVEL"] ?: "master"
+                              println "Making job $jobName with a target manifest using branch: $targetBranch"
+                              branch(targetBranch)
+                            }
+                        }
+                    }
+
                     agent { label 'master' }
                     environment {
                         success = hoboWebCheck("http://other-widget.jx-go2group-other-widget-pr-5.35.196.145.46.nip.io", "Cool Jenkins X demo")
